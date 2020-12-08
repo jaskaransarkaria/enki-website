e0ee7c8780973a242542bb00009d9f0d32564a8b
jaskaran <jaskaran.sarkaria@googlemail.com>
2020-12-08 19:26:04 +0000

    feat: 🎸 stage stuff

--- src/pages/contact.svelte
+++ src/pages/contact.svelte
@@ -2,7 +2,6 @@
   import { url } from '@roxi/routify';
 </script>
 
-
 <style>
   .contact-us-container {
     display: flex;
@@ -35,7 +34,17 @@
 <div class="contact-us-container">
   <img src="/faith.jpg" alt="shop creator and jeweller" />
   <div class="contact-description">
-    <p>Enki was started by Faith, a Birmingham trained jeweller who as well as repairing and remodeling old jewellery offers personal service creating bespoke jewellery using traditional techniques just for you.</p>
-    <p>The Kings Heath store shows the work of many independent crafts people who have become almost invisible on the High Street today. From baby clothes to pottery, jewellery to cushions, Enki has just the gift you're looking for. They also sell beautiful and unique cards and wrapping paper to make your present that bit more special.</p>
+    <p>
+      Enki was started by Faith, a Birmingham trained jeweller who as well as
+      repairing and remodeling old jewellery offers personal service creating
+      bespoke jewellery using traditional techniques just for you.
+    </p>
+    <p>
+      The Kings Heath store shows the work of many independent crafts people who
+      have become almost invisible on the High Street today. From baby clothes
+      to pottery, jewellery to cushions, Enki has just the gift you're looking
+      for. They also sell beautiful and unique cards and wrapping paper to make
+      your present that bit more special.
+    </p>
   </div>
 </div>
