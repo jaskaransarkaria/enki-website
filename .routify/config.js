0568d4824d8573795a0a1374a6a0c532dc18abfd
jaskaran <jaskaran.sarkaria@googlemail.com>
2021-01-05 15:49:28 +0000

    fix: 🐛 jewellery store {} -> []

--- .routify/config.js
+++ .routify/config.js
@@ -1,12 +1,17 @@
 module.exports = {
-  pages: 'src/pages',
-  sourceDir: 'public',
-  routifyDir: '.routify',
-  ignore: '',
-  dynamicImports: true,
-  singleBuild: false,
-  noHashScroll: false,
-  distDir: 'dist',
-  extensions: ['svelte', 'html', 'svx', 'md'],
-  started: '2021-01-05T14:38:49.398Z',
-};
+  "pages": "src/pages",
+  "sourceDir": "public",
+  "routifyDir": ".routify",
+  "ignore": "",
+  "dynamicImports": true,
+  "singleBuild": false,
+  "noHashScroll": false,
+  "distDir": "dist",
+  "extensions": [
+    "svelte",
+    "html",
+    "svx",
+    "md"
+  ],
+  "started": "2021-01-05T15:47:29.802Z"
+}
--- src/stores/jewellery.ts
+++ src/stores/jewellery.ts
@@ -1,3 +1,3 @@
 import { writable } from 'svelte/store';
 
-export const jewellery = writable({});
+export const jewellery = writable([]);
