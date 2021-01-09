fc935569eb7ce78ce4925278f42be9cb201cea65
jaskaran <jaskaran.sarkaria@googlemail.com>
2021-01-09 18:16:21 +0000

    refactor: 💡 fp-ts

--- .routify/config.js
+++ .routify/config.js
@@ -1,17 +1,12 @@
 module.exports = {
-  "pages": "src/pages",
-  "sourceDir": "public",
-  "routifyDir": ".routify",
-  "ignore": "",
-  "dynamicImports": true,
-  "singleBuild": false,
-  "noHashScroll": false,
-  "distDir": "dist",
-  "extensions": [
-    "svelte",
-    "html",
-    "svx",
-    "md"
-  ],
-  "started": "2021-01-09T15:34:36.202Z"
-}
+  pages: 'src/pages',
+  sourceDir: 'public',
+  routifyDir: '.routify',
+  ignore: '',
+  dynamicImports: true,
+  singleBuild: false,
+  noHashScroll: false,
+  distDir: 'dist',
+  extensions: ['svelte', 'html', 'svx', 'md'],
+  started: '2021-01-09T15:34:36.202Z',
+};
--- src/components/ProductView/ProductView.svelte
+++ src/components/ProductView/ProductView.svelte
@@ -2,99 +2,39 @@
   import { goto } from '@roxi/routify';
   import { onMount } from 'svelte';
   import AddToBasket from '../AddToBasket/AddToBasket.svelte';
-  import { identity, pipe } from 'fp-ts/lib/function'
-  import * as TE from 'fp-ts/lib/TaskEither'
+  import { flow, pipe } from 'fp-ts/lib/function';
+  import * as TE from 'fp-ts/lib/TaskEither';
+  import * as T from 'fp-ts/lib/Task';
+  import * as E from 'fp-ts/lib/Either';
+  
 
 
   export let categoryId: string;
 
-  let productArr: object[] = [];
+  let productArr: unknown = [];
 
   onMount(async () => {
     // fetch all the products in a specific category
-
-    // A task is a function that returns a promise which is expected to never be rejected.
-    // Tasks are expected to always succeed but can fail when an error occurs outside our 
-    // expectations. In this case, the error is thrown and breaks the functional pipeline.
-    // An analogy to this is awaiting a Promise that throws an error without putting a try-catch-finally block in front.
-
-//     Any arbitrary value can become a Task by using the of operator to lift it into the Task world.
-//  This is equivalent to calling Promise.resolve.
-
-// import * as T from 'fp-ts/lib/Task'
-
-// const foo = 'asdf' // string
-// const bar = T.of(foo) // T.Task<string>
-
-// // Same As
-// const fdsa: T.Task<string> = () => Promise.resolve(foo)
-
-// An Either is a type that represents a synchronous operation that can succeed or fail. Much like Option, where 
-// it is Some or None, the Either type is either Right or Left. Right represents success and Left represents failure.
-// The Either type is a union type of Left and Right.
-
-// Eithers are essential for capturing error states in functional programming. We need the Eithers because we cannot break pipelines by throwing errors. Error states must either be handled or propagated up the call stack.
-
-// Eithers are also advantageous to their try-catch-finally counterparts because the error is always type-safe. When you use a catch block, the error is always of type unknown. This is inconvenient for you as the client because you need to use instanceof to narrow down the error type.
-
-// With Eithers, we know every possible error state based on the type signature. We can choose to handle them in a switch statement or continue to propagate up the call stack.
-
-// TaskEither is an asynchronous operation that can fail.
-
-// import { pipe } from 'fp-ts/lib/function'
-// import * as TE from 'fp-ts/lib/TaskEither'
-// ;(async () => {
-//   const ok = await pipe(
-//     TE.tryCatch(
-//       () => axios.get('https://httpstat.us/200'),
-//       (reason) => new Error(`${reason}`),
-//     ),
-//     TE.map((resp) => resp.data),
-//   )()
-
-//   console.log(ok)
-//   // { _tag: 'Right', right: { code: 200, description: 'OK' } }
-// })()
-
-    const fetchProductsByCategoryIdFunctional = await pipe(
-      TE.tryCatch(
-        () => fetch(`${process.env.SERVER_URL}/products-by-category?id=${categoryId}`),
-        (reason) => new Error(`${reason}`),
-      ),
-      await TE.map(async (resp: any) => {
-        const result = await resp.json()
-        return result
-      })
-      )
-
-
-  function get(url: string): TE.TaskEither<Error, any> {
-    return TE.tryCatch(() => fetch(url).then(res => res.json()), reason => new Error(String(reason)))
-  }
-
-    // const fetchProductsByCategoryIdFunctional = async () => {
-    //   const ok = await pipe(
-    //   TE.tryCatch(
-    //     () => fetch(`${process.env.SERVER_URL}/products-by-category?id=${categoryId}`),
-    //     (reason) => new Error(`${reason}`),
-    //   ),
-    //   TE.map((resp) => resp.json())
-    //   )()
-    //   console.log(ok)
-    // }
-
-    const letsSee = await get(`${process.env.SERVER_URL}/products-by-category?id=${categoryId}`)()
-    const unpack = await letsSee
-    console.log("lets", unpack)
-
-    const fetchProductsByCategoryId = async (id: string) => {
-      const response = await fetch(
-        `${process.env.SERVER_URL}/products-by-category?id=${id}`
+    const get = (url: string): TE.TaskEither<Error, object[]> => TE.tryCatch(
+        () => fetch(url).then((res) => res.json()),
+        (reason) => new Error(String(reason))
       );
-      const result = await response.json();
-      return result;
-    };
-    productArr = await fetchProductsByCategoryId(categoryId);
+
+    // cast value to Either Left or Right
+    const validateResponse = () => (response: any): E.Either<Error, any> => (response ? E.right(response) : E.left(Error('Invalid api call!')));
+
+
+    const getProductsByCategory = await pipe(
+      get(
+      `${process.env.SERVER_URL}/products-by-category?id=${categoryId}`
+    ),
+    TE.chain(flow(validateResponse(), TE.fromEither)),
+    TE.getOrElse((e => TE.of(e)) // get value or return the error
+    ));
+
+    const products = await getProductsByCategory()
+
+    productArr = (products instanceof Error) ? [] : products;
   });
 </script>
 
