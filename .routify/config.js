548e93100720542a6c87214ad8ff46a126bd454e
jaskaransarkaria <jaskaran.sarkaria@googlemail.com>
2021-07-27 10:42:39 +0100

    ci: 🎡 node version bump

--- .routify/config.js
+++ .routify/config.js
@@ -8,5 +8,5 @@ module.exports = {
   noHashScroll: false,
   distDir: 'dist',
   extensions: ['svelte', 'html', 'svx', 'md'],
-  started: '2021-07-27T09:21:41.056Z',
+  started: '2021-07-27T09:41:44.317Z',
 };
