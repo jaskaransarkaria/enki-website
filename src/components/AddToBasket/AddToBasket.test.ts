c3a2d42ca3f9ebf1af57e96c9561f6ff0e13cb59
Jaskaran Sarkaria <jaskaran.sarkaria@googlemail.com>
2021-10-29 20:04:32 +0100

    test: 💍 skip

--- src/components/AddToBasket/AddToBasket.test.ts
+++ src/components/AddToBasket/AddToBasket.test.ts
@@ -61,62 +61,71 @@ describe('GIVEN AddToBasket', () => {
     });
 
     // move this test logic to libs/basket || pages/online-shop/basket
-    //it('THEN remove items from the basket', async () => {
-    //  render(AddToBasket, {
-    //    productId: 1,
-    //  });
+    it.skip('THEN remove items from the basket', async () => {
+      render(AddToBasket, {
+        productId: 1,
+      });
 
-    //  expect(get(basket)).toEqual([]);
-    //  userEvent.click(screen.getByRole('button', { name: /add to basket/i }));
-    //  expect(get(basket)).toEqual([{ id: '1', quantity: 1 }]);
-    //  userEvent.click(screen.getByRole('button', { name: /add to basket/i }));
-    //  expect(get(basket)).toEqual([{ id: '1', quantity: 2 }]);
-    //  await tick();
-    //  userEvent.click(
-    //    screen.getByRole('button', { name: /remove from basket/i })
-    //  );
-    //  expect(get(basket)).toEqual([{ id: '1', quantity: 1 }]);
-    //  userEvent.click(
-    //    screen.getByRole('button', { name: /remove from basket/i })
-    //  );
-    //  expect(get(basket)).toEqual([]);
-    //});
+      expect(get(basket)).toEqual([]);
+      userEvent.click(screen.getByRole('button', { name: /add to basket/i }));
+      expect(get(basket)).toEqual([{ id: '1', quantity: 1 }]);
+      userEvent.click(screen.getByRole('button', { name: /add to basket/i }));
+      expect(get(basket)).toEqual([{ id: '1', quantity: 2 }]);
+      await tick();
+      userEvent.click(
+        screen.getByRole('button', { name: /remove from basket/i })
+      );
+      expect(get(basket)).toEqual([{ id: '1', quantity: 1 }]);
+      userEvent.click(
+        screen.getByRole('button', { name: /remove from basket/i })
+      );
+      expect(get(basket)).toEqual([]);
+    });
 
-    //it('THEN add a new item to the basket', async () => {
-    //  const { container } = render(AddToBasket, {
-    //  product: {
-    //     Id: 123,
-    //     Name: 'Red jacket',
-    //     SalePrice: 1000,
-    //     CurrentStock: 1,
-    //   },
-    //  });
+    // move this test to libs/basket || pages/online-shop/basket
+    it.skip('THEN add a new item to the basket', async () => {
+      const { container } = render(AddToBasket, {
+        product: {
+          Id: 123,
+          Name: 'Red jacket',
+          SalePrice: 1000,
+          CurrentStock: 1,
+        },
+      });
 
-    //  expect(get(basket)).toEqual([]);
-    //  userEvent.click(screen.getByRole('button', { name: /add to basket/i }));
-    //  expect(get(basket)).toEqual([{ id: '123', quantity: 1, name: "Red jacket", price: 1000, currentStock: 1 }]);
+      expect(get(basket)).toEqual([]);
+      userEvent.click(screen.getByRole('button', { name: /add to basket/i }));
+      expect(get(basket)).toEqual([
+        {
+          id: '123',
+          quantity: 1,
+          name: 'Red jacket',
+          price: 1000,
+          currentStock: 1,
+        },
+      ]);
 
-    //  render(AddToBasket, {
-    //    target: container,
-    //    props: {
-    //      product: {
-    //         Id: 456,
-    //         Name: 'Blue jacket',
-    //       },
-    //    },
-    //  });
-    //  await tick()
-    //  await tick()
-    //  await tick()
-    //  await tick()
-    //    console.log(screen.getAllByRole('button', { name: /add to basket/i }))
-    //  userEvent.click(
-    //    screen.getAllByRole('button', { name: /add to basket/i })[1]
-    //  );
-    //  expect(get(basket)).toMatchObject([
-    //    { id: '456', quantity: 1 },
-    //    { id: '123', quantity: 1 },
-    //  ]);
-    //});
+      render(AddToBasket, {
+        target: container,
+        props: {
+          product: {
+            Id: 456,
+            Name: 'Blue jacket',
+          },
+        },
+      });
+      await tick();
+      await tick();
+      await tick();
+      await tick();
+      console.log(screen.getAllByRole('button', { name: /add to basket/i }));
+      userEvent.click(
+        screen.getAllByRole('button', { name: /add to basket/i })[1]
+      );
+      expect(get(basket)).toMatchObject([
+        { id: '456', quantity: 1 },
+        { id: '123', quantity: 1 },
+      ]);
+    });
   });
 });
