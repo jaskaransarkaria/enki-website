1bf44ea6e788eeac15d690bd9d32389f19e31b07
Jaskaran Sarkaria <jaskaran.sarkaria@googlemail.com>
2021-10-30 17:01:17 +0100

    fix: 🐛 remove basketcounter from tests

--- jest.config.cjs
+++ jest.config.cjs
@@ -25,7 +25,7 @@ module.exports = {
     'src/**/*',
     '!src/main.ts',
     '!src/App.svelte',
-    '!src/components/BasketCounter.svelte',
+    '!src/components/BasketCounter/*',
     '!src/components/Navbar/*',
     '!src/mocks/**/*',
     '!src/pages/**/*',
