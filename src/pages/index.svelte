f9e48939f442abbc4ed29164d147ff885cc5577d
Jaskaran Sarkaria <jaskaran.sarkaria@googlemail.com>
2022-01-26 20:20:47 +0000

    style: 💄 front page mobile

--- src/pages/index.svelte
+++ src/pages/index.svelte
@@ -11,7 +11,13 @@
 <svelte:window bind:outerWidth />
 <figure>
   <img
-    src={`http://enki.imgix.net/inside-enki.png?fit=crop&crop=focalpoint&fp-x=0.525&fp-y=0.3&fp-z=1.4&w=${outerWidth}`}
+    src={`http://enki.imgix.net/inside-enki${
+      outerWidth <= 360 ? '-mobile' : ''
+    }.png?${
+      outerWidth <= 360
+        ? 'fit=crop'
+        : 'fit=crop&crop=focalpoint&fp-x=0.525&fp-y=0.3&fp-z=1.4'
+    }&w=${outerWidth}`}
     alt="welcome to the shop, this ilustration shows the shopkeeper behind her bench"
   />
 </figure>
