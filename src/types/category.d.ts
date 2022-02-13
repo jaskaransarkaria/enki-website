c5929993f36e1a2408e8330a9af6b84ef304f4aa
jaskaran <jaskaran.sarkaria@googlemail.com>
2022-02-13 15:19:23 +0000

    add variant comment

--- src/types/category.d.ts
+++ src/types/category.d.ts
@@ -1,7 +1,7 @@
 import type { Base } from '@/types/base';
 
 export interface Category extends Base {
-  CategoryId?: number;
+  CategoryId?: number; // if its a variant
   ParentId: number | null;
   Children: Category[];
   NominalCode: string | null;
