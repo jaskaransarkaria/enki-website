68efde58e117114c8f9165962ff8fc3a53f7c2d7
Jaskaran Sarkaria <jaskaran.sarkaria@googlemail.com>
2022-02-15 20:49:04 +0000

    fix: 🐛 search error

--- src/libs/search.ts
+++ src/libs/search.ts
@@ -4,4 +4,4 @@ export const searchProducts = (
   prodToFind: RegExp,
   productsArr: readonly Product[]
 ): readonly Product[] =>
-  productsArr.filter((obj) => obj.Name.toLowerCase().match(prodToFind));
+  productsArr.filter((obj) => obj?.Name.toLowerCase().match(prodToFind));
