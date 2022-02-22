c31e093f4ddb9d2a6985cb64d47601570fb7191e
Jaskaran Sarkaria <jaskaran.sarkaria@googlemail.com>
2022-02-22 17:25:39 +0000

    test: 💍 update root categories test

--- src/components/RootCategories/RootCategories.test.ts
+++ src/components/RootCategories/RootCategories.test.ts
@@ -1,15 +1,57 @@
 import '@testing-library/jest-dom';
-import { render, screen } from '@testing-library/svelte';
+import { render, screen, cleanup } from '@testing-library/svelte';
+import { get } from 'svelte/store';
 import RootCategories from './RootCategories.svelte';
 import { refreshCategories } from '@/libs/requests';
-import { resetReadonlyAllCategoriesStore } from '@/stores/categories';
+import {
+  readonlyAllCategories,
+  resetReadonlyAllCategoriesStore,
+} from '@/stores/categories';
 
 jest.mock('@/libs/requests');
+
+const nestedCategories = {
+  Id: 111,
+  ParentId: null,
+  Name: 'ROOT',
+  NominalCode: 'CATEGORY',
+  Children: [
+    {
+      Id: 123,
+      ParentId: null,
+      Name: 'Clothes',
+      NominalCode: 'CATEGORY',
+      Children: [
+        {
+          Id: 456,
+          ParentId: null,
+          Name: 'Shoes',
+          NominalCode: 'CATEGORY',
+          Children: [
+            {
+              Id: 789,
+              ParentId: null,
+              Name: 'Hats',
+              NominalCode: 'CATEGORY',
+
+              Children: [],
+            },
+          ],
+        },
+      ],
+    },
+  ],
+};
 
 describe('GIVEN RootCategories', () => {
   beforeEach(() => {
     (refreshCategories as jest.Mock).mockResolvedValue([]);
     resetReadonlyAllCategoriesStore();
+    cleanup();
+  });
+
+  afterEach(() => {
+    jest.clearAllMocks();
   });
 
   describe('WHEN rendered', () => {
@@ -26,63 +68,80 @@ describe('GIVEN RootCategories', () => {
 
       expect(screen.getByTestId('loading-spinner')).toBeInTheDocument();
     });
-  });
 
-  describe('WHEN handling category data', () => {
-    it('THEN fetch categories from api AND display them', async () => {
-      (refreshCategories as jest.Mock).mockResolvedValueOnce([
-        {
-          Id: 123,
-          ParentId: -1,
-          Name: 'Clothes',
-          Children: [],
+    describe('WHEN we already have the category data', () => {
+      it("THEN don't fetch it again from the api", async () => {
+        readonlyAllCategories.set([nestedCategories]);
+        render(RootCategories);
 
-          NominalCode: '',
-        },
-      ]);
+        expect(refreshCategories).not.toHaveBeenCalled();
+        expect(get(readonlyAllCategories)).toEqual([nestedCategories]);
 
-      render(RootCategories);
-
-      expect(refreshCategories).toHaveBeenCalled();
-      expect(await screen.findByTestId('hex-category-name')).toHaveTextContent(
-        'Clothes'
-      );
-      expect(
-        await screen.queryByTestId('loading-spinner')
-      ).not.toBeInTheDocument();
+        expect(screen.getByTestId('hex-category-name')).toHaveTextContent(
+          'ROOT'
+        );
+        expect(
+          await screen.queryByTestId('loading-spinner')
+        ).not.toBeInTheDocument();
+      });
     });
 
-    it('THEN order categories alphabetically', async () => {
-      (refreshCategories as jest.Mock).mockResolvedValueOnce([
-        {
-          Id: 123,
-          ParentId: -1,
-          Name: 'Clothes',
-          Children: [],
+    describe('WHEN handling category data', () => {
+      it('THEN fetch categories from api AND display them', async () => {
+        (refreshCategories as jest.Mock).mockResolvedValueOnce([
+          {
+            Id: 123,
+            ParentId: -1,
+            Name: 'Clothes',
+            Children: [],
 
-          NominalCode: '',
-        },
-        {
-          Id: 456,
-          ParentId: -1,
-          Name: 'Books',
-          Children: [],
+            NominalCode: '',
+          },
+        ]);
 
-          NominalCode: '',
-        },
-      ]);
+        render(RootCategories);
 
-      render(RootCategories);
+        expect(refreshCategories).toHaveBeenCalled();
+        expect(
+          await screen.findByTestId('hex-category-name')
+        ).toHaveTextContent('Clothes');
+        expect(
+          await screen.queryByTestId('loading-spinner')
+        ).not.toBeInTheDocument();
+      });
 
-      expect(refreshCategories).toHaveBeenCalled();
+      it('THEN order categories alphabetically', async () => {
+        (refreshCategories as jest.Mock).mockResolvedValueOnce([
+          {
+            Id: 123,
+            ParentId: -1,
+            Name: 'Clothes',
+            Children: [],
 
-      const buttons = await screen.findAllByTestId('hex-category-name');
+            NominalCode: '',
+          },
+          {
+            Id: 456,
+            ParentId: -1,
+            Name: 'Books',
+            Children: [],
 
-      expect(buttons[0]).toHaveTextContent('Books');
-      expect(buttons[1]).toHaveTextContent('Clothes');
-      expect(
-        await screen.queryByTestId('loading-spinner')
-      ).not.toBeInTheDocument();
+            NominalCode: '',
+          },
+        ]);
+
+        render(RootCategories);
+
+        expect(refreshCategories).toHaveBeenCalled();
+
+        const buttons = await screen.findAllByTestId('hex-category-name');
+
+        expect(buttons[0]).toHaveTextContent('Books');
+        expect(buttons[1]).toHaveTextContent('Clothes');
+        expect(
+          await screen.queryByTestId('loading-spinner')
+        ).not.toBeInTheDocument();
+      });
     });
   });
 });
