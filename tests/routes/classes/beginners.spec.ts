9d6aa6816416d68ef86050edf47502996d6d1fe2
Jaskaran Sarkaria <jaskaran.sarkaria@googlemail.com>
2022-04-01 19:35:38 +0100

    travis

--- tests/routes/classes/beginners.spec.ts
+++ tests/routes/classes/beginners.spec.ts
@@ -2,14 +2,22 @@ import { expect, test } from "@playwright/test";
 
 test("Classes Index Page", async ({ page }) => {
   await page.goto("/classes/beginners")
-  await expect(page.locator(".class-button > img >> nth=0")).toHaveAttribute("src", "https://enki.imgix.net/group_portrait.png?auto=format,compress")
-  await page.locator(".class-button > img >> nth=0").click()
-  await page.waitForURL("http://localhost:4000/classes/beginners")
-  await page.goBack()
-  await page.waitForURL("http://localhost:4000/classes")
-  await expect(page.locator(".class-button > img >> nth=1")).toHaveAttribute("src", "https://enki.imgix.net/wedding_portrait.png?auto=format,compress")
-  await page.locator(".class-button > img >> nth=1").click()
-  await page.waitForURL("http://localhost:4000/classes/wedding-rings")
+  await expect(page.locator(".class-pics > img >> nth=0")).toHaveAttribute("src", "https://enki.imgix.net/classes_example_1.jpg?auto=format,compress")
+  await expect(page.locator(".class-pics > img >> nth=1")).toHaveAttribute("src", "https://enki.imgix.net/classes_example_2.jpg?auto=format,compress")
+  await expect(page.locator(".class-pics > img >> nth=2")).toHaveAttribute("src", "https://enki.imgix.net/classes_example_3.jpg?auto=format,compress")
+  await page.locator("here").click()
+  await page.waitForURL("http://localhost:4000/contact")
   await page.goBack()
   await page.waitForURL("http://localhost:4000/classes/beginners")
+
+  await expect(page.locator(".class-pics > p")).toHaveText(`Ever wanted to learn to make your own jewellery? Looking for a unique
+    present for a friend? <br /><br />At Enki workshops you can meet new people
+    and learn a new craft. The one off classes include all the tools and
+    materials needed to make your jewellery, soft and hot drinks, and cake from
+    the wonderful Early Bird Bakery next door. <br /><br />We also take group
+    bookings where you can choose the date and topic, get in contact
+    here with any enquiries.`)
+
+  await expect(page.locator(".tt-widget")).toHaveCount(1)
+  await expect(page.locator(".tt-widget-fallback")).toBeHidden()
 })
