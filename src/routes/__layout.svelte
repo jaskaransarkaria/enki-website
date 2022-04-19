3ec45815ea951fad4a3d905d2eaf2ab22d804bcc
Jaskaran Sarkaria <jaskaran.sarkaria@googlemail.com>
2022-04-19 21:00:30 +0100

    style: 💄 the browser tab

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -18,6 +18,12 @@
     content="Independent jewellers and craft gallery -- Kings Heath Bimringham"
   />
   <meta property="og:locale" content="en_GB" />
+  <!-- Chrome, Firefox OS and Opera -->
+  <meta name="theme-color" content="#008080" />
+  <!-- Windows Phone -->
+  <meta name="msapplication-navbutton-color" content="#008080" />
+  <!-- iOS Safari -->
+  <meta name="apple-mobile-web-app-status-bar-style" content="#4285f4" />
 </svelte:head>
 
 <div class="container">
