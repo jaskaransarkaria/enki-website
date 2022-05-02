16e4b3845561265a0c0a5a90f0924ad79449e745
Jaskaran Sarkaria <jaskaran.sarkaria@googlemail.com>
2022-05-02 17:20:42 +0100

    style: 💄 mobile buttons

--- src/lib/utils/consts.js
+++ src/lib/utils/consts.js
@@ -1,15 +1,15 @@
 export const MOBILE_LANDING_PAGE =
   "https://enki.imgix.net/mobile_landing_page_2.png?fit=crop";
 export const MOBILE_ONLINE_SHOP =
-  "https://enki.imgix.net/mobile_online_shop.png?auto=format,compress";
+  "https://enki.imgix.net/mobile_online_shop_3.png?auto=format,compress";
 export const MOBILE_JEWELLERY_CLASSES =
-  "https://enki.imgix.net/mobile_jewellery_classes.png?auto=format,compress";
+  "https://enki.imgix.net/mobile_jewellery_classes_3.png?auto=format,compress";
 export const MOBILE_JEWELLERY_SERVICES =
-  "https://enki.imgix.net/mobile_jewellery_services.png?auto=format,compress";
+  "https://enki.imgix.net/mobile_jewellery_repairs_3.png?auto=format,compress";
 export const MOBILE_CONTACT_US =
-  "https://enki.imgix.net/mobile_contact_us.png?auto=format,compress";
+  "https://enki.imgix.net/mobile_contact_us_3.png?auto=format,compress";
 export const MOBILE_ABOUT_US =
-  "https://enki.imgix.net/mobile_about_us.png?auto=format,compress";
+  "https://enki.imgix.net/mobile_about_us_3.png?auto=format,compress";
 export const DESKTOP_LANDING_PAGE =
   "https://enki.imgix.net/inside_enki_desktop_3.png?auto=format,compress";
 
