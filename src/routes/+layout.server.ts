555b55e46b28f3be9ce9692e5353de47e04b696f
Jaskaran Sarkaria <jaskaran.sarkaria@googlemail.com>
2022-08-20 22:23:33 +0100

    style: 💄 remove log

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -3,7 +3,6 @@ import { isWhitelistedUserAgent } from "$lib/utils/consts";
 export async function load({ request, data }) {
   const userAgent = request?.headers.get("User-Agent");
 
-  console.log("ua", userAgent);
   return {
     whitelistedUserAgent: isWhitelistedUserAgent(userAgent),
   };
