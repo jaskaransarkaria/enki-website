7371cf4462130ac382ecd1ed3d6eb776b4553603
Jaskaran Sarkaria <jaskaran.sarkaria@googlemail.com>
2022-09-16 08:42:17 +0100

    fix: 🐛 IsArchived logic

--- src/lib/utils/search.ts
+++ src/lib/utils/search.ts
@@ -4,14 +4,15 @@ export const searchProducts = (
   prodToFind: RegExp,
   productsArr: readonly Product[]
 ): readonly Product[] =>
-  productsArr?.filter((obj) =>
-    "Name" in obj
-      ? (obj?.Name.toLowerCase().match(prodToFind) ||
-          obj?.Description.toLowerCase().match(prodToFind) ||
-          obj?.ProductDetails?.DetailedDescription.toLowerCase().match(
-            prodToFind
-          )) &&
-        obj.SellOnWeb &&
-        !obj.IsArchived
-      : false
-  );
+  productsArr
+    ?.filter((obj) =>
+      "Name" in obj
+        ? (obj?.Name.toLowerCase().match(prodToFind) ||
+            obj?.Description.toLowerCase().match(prodToFind) ||
+            obj?.ProductDetails?.DetailedDescription.toLowerCase().match(
+              prodToFind
+            )) &&
+          obj.SellOnWeb
+        : false
+    )
+    .filter((obj) => !obj.IsArchived);
