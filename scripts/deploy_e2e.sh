73fe7243b5afc5dda9bec5b09773e556b1e64b85
Jaskaran Sarkaria <jaskaran.sarkaria@googlemail.com>
2023-04-16 20:17:28 +0100

    feat: 🎸 update deps and add /useful-links (link for qr code) (#16)

---
+++ scripts/deploy_e2e.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+set -eux
+
+docker build -t jaskaransarkaria/enki-website:e2e-test-${E2E_VERSION_NUM} . 
+
+docker push jaskaransarkaria/enki-website:e2e-test-${E2E_VERSION_NUM}
+
+sed -i "s/replace_with_git_tag/e2e-test-$E2E_VERSION_NUM/" .kubernetes/e2e-test-deployment.yaml
+
+kubectl apply -f .kubernetes/e2e-test-deployment.yaml
+
+sed -i "s/e2e-test-$E2E_VERSION_NUM/replace_with_git_tag/" .kubernetes/e2e-test-deployment.yaml
+
+exit 0
+
