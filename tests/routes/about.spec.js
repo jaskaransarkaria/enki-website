e4986bfc79a86d2f34e005f4ad5eaf553700e5b5
Jaskaran Sarkaria <jaskaran.sarkaria@googlemail.com>
2023-12-31 11:57:04 +0000

    test: 💍 add timeout (#29)

--- tests/routes/about.spec.js
+++ tests/routes/about.spec.js
@@ -2,5 +2,6 @@ import { expect, test } from "@playwright/test";
 
 test("About Page", async ({ page }) => {
   await page.goto("/about");
+  await page.waitForTimeout(1000);
   await expect(page).toHaveScreenshot({ maxDiffPixelRatio: 0.1 });
 });
