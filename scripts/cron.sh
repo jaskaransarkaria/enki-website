f6048590e8d1aab181c8968939eb4c77c0b4edc6
Jaskaran Sarkaria <jaskaran.sarkaria@googlemail.com>
2025-05-26 21:25:05 +0100

    fix: 🐛 priority map

--- scripts/cron.sh
+++ scripts/cron.sh
@@ -2,7 +2,7 @@
 
 set -ex
 
-npx sitemap-generator-cli https://enkionline.com --last-mod --change-freq weekly --priority-map \"1.0\" --user-agent sitemap-generator-cli --filepath sitemap.xml --max-concurrency 10 --verbose --query true
+npx sitemap-generator-cli https://enkionline.com --last-mod --change-freq weekly --priority-map "1.0" --user-agent sitemap-generator-cli --filepath sitemap.xml --max-concurrency 10 --verbose --query true
 
 kubectl create configmap sitemap --from-file=sitemap.xml -o yaml --dry-run | kubectl replace -f -
 
