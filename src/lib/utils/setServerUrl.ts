d3ff7893564fc704d6b9aaa850dffc7fc0a5037b
jaskaran <jaskaran.sarkaria@googlemail.com>
2025-12-18 18:13:17 +0000

    fix: 🐛 internal k8s endpoint

--- src/lib/utils/setServerUrl.ts
+++ src/lib/utils/setServerUrl.ts
@@ -1,4 +1,4 @@
-import { PUBLIC_SERVER_URL } from "$env/static/public";
+import { PUBLIC_SERVER_URL, PUBLIC_INTERNAL_K8S_URL } from "$env/static/public";
 
 export const setServerUrl = (isBrowser: boolean, isDev: boolean): string =>
-  isBrowser || isDev ? PUBLIC_SERVER_URL : PUBLIC_SERVER_URL;
+  isBrowser || isDev ? PUBLIC_SERVER_URL : PUBLIC_INTERNAL_K8S_URL;
